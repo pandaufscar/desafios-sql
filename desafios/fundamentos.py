613d6cca53abee08a2d31a09c362f49664c663d2
Lucas Battisti <lucas.sala.battisti.pessoal@gmail.com>
2026-03-01 18:20:25 -0300

    desafios de fundamentos

--- desafios/fundamentos.py
+++ desafios/fundamentos.py
@@ -16,4 +16,34 @@ dic1 = {
     "resposta_sql": resposta_sql
 }
 
-dics = [dic1]
+titulo = "Produtos com estoque baixo"
+enunciado = "Liste o nome e o estoque dos produtos com estoque menor que 50."
+imagem_esquema = "schema.png"
+resposta_sql = """
+SELECT nome, estoque
+FROM produtos
+WHERE estoque < 50
+"""
+dic2 = {
+    "titulo": titulo,
+    "enunciado": enunciado,
+    "imagem_esquema": imagem_esquema,
+    "resposta_sql": resposta_sql
+}
+
+titulo = "Clientes nascidos no século 21"
+enunciado = "Liste o nome dos clientes que nasceram no século 21."
+imagem_esquema = "schema.png"
+resposta_sql = """
+SELECT nome
+FROM clientes
+WHERE data_nascimento >= '2001-01-01'
+"""
+dic3 = {
+    "titulo": titulo,
+    "enunciado": enunciado,
+    "imagem_esquema": imagem_esquema,
+    "resposta_sql": resposta_sql
+}
+
+dics = [dic1, dic2, dic3]
