312f6e95bc96011c34ec3e85252dcd398fc277fd
Lucas Battisti <lucas.sala.battisti.pessoal@gmail.com>
2026-03-25 13:57:33 -0300

    exercicios fundamentos adicionados

--- desafios/fundamentos.py
+++ desafios/fundamentos.py
@@ -1,4 +1,4 @@
-titulo = "Selecionando colunas"
+titulo = "1. Selecionando colunas"
 
 enunciado = "Liste o nome e a cidade de todos os clientes."
 
@@ -17,13 +17,13 @@ dic1 = {
     "ordem": False
 }
 
-titulo = "Produtos com estoque baixo"
-enunciado = "Liste o nome e o estoque dos produtos com estoque menor que 50."
+titulo = "2. Produtos que valem mais de 50 reais"
+enunciado = "Liste nome e preço dos produtos com preço maior que 50."
 imagem_esquema = "schema.jpg"
 resposta_sql = """
-SELECT nome, estoque
-FROM produtos
-WHERE estoque < 50
+SELECT nome, preco
+FROM Produtos
+WHERE preco > 50
 """
 dic2 = {
     "titulo": titulo,
@@ -33,7 +33,7 @@ dic2 = {
     "ordem": False
 }
 
-titulo = "Clientes nascidos no século 21"
+titulo = "3. Clientes nascidos no século 21"
 enunciado = "Liste o nome dos clientes que nasceram no século 21."
 imagem_esquema = "schema.jpg"
 resposta_sql = """
@@ -49,4 +49,53 @@ dic3 = {
     "ordem": False
 }
 
-dics = [dic1, dic2, dic3]
+titulo = "4. Filtre por estoque e preço"
+enunciado = "Liste o nome, preço e estoque dos produtos com preço maior que 30 e estoque maior que 5."
+imagem_esquema = "schema.jpg"
+resposta_sql = """
+SELECT nome, preco, estoque
+FROM Produtos
+WHERE preco > 30 AND estoque > 5
+"""
+dic4 = {
+    "titulo": titulo,
+    "enunciado": enunciado,
+    "imagem_esquema": imagem_esquema,
+    "resposta_sql": resposta_sql,
+    "ordem": False
+}
+
+titulo = "5. Ordem alfabética"
+enunciado = "Liste o nome dos clientes em ordem alfabética."
+imagem_esquema = "schema.jpg"
+resposta_sql = """
+SELECT nome
+FROM Clientes
+ORDER BY nome ASC
+"""
+dic5 = {
+    "titulo": titulo,
+    "enunciado": enunciado,
+    "imagem_esquema": imagem_esquema,
+    "resposta_sql": resposta_sql,
+    "ordem": True
+}
+
+titulo = "6. Primeiros pedidos"
+enunciado = "Mostre os id's dos 5 primeiros pedidos feitos na livraria."
+imagem_esquema = "schema.jpg"
+resposta_sql = """
+SELECT id_pedido
+FROM Pedidos
+ORDER BY data_pedido
+LIMIT 5
+"""
+dic6 = {
+    "titulo": titulo,
+    "enunciado": enunciado,
+    "imagem_esquema": imagem_esquema,
+    "resposta_sql": resposta_sql,
+    "ordem": True
+}
+
+dics = [dic1, dic2, dic3, dic4, dic5, dic6]
